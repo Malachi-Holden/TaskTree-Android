5c8ad864d88d64f123a28dee3b004c8e4c59a4b2
Malachi <malachi.coqui@gmail.com>
2023-11-04 14:28:24 -0400

    Sample repo and tests

---
+++ app/src/main/java/com/holden/tasktree_android/task/SampleRepo.kt
@@ -0,0 +1,45 @@
+package com.holden.tasktree_android.task
+
+import kotlinx.coroutines.delay
+
+class SampleRepo: TaskRepository {
+    private val DELAY = 300L
+    val tasks: MutableMap<String, Task> = mutableMapOf()
+    val parentTasks: MutableList<String> = mutableListOf()
+
+    override suspend fun getParentTasks(): TaskListResult {
+        delay(DELAY)
+        return TaskListResult.Success(parentTasks.mapNotNull { tasks[it] })
+    }
+
+    override suspend fun getSubTasks(parentId: String): TaskListResult {
+        delay(DELAY)
+        val task = tasks[parentId] ?: return TaskListResult.Failure(IllegalArgumentException("No task with id: $parentId"))
+        return TaskListResult.Success(task.subTasks)
+    }
+
+    override suspend fun updateTask(task: Task): TaskUpdateResult {
+        return if (tasks.containsKey(task.id)) {
+            tasks[task.id] = task
+            TaskUpdateResult.Success
+        } else {
+            TaskUpdateResult.Failure(IllegalArgumentException("No task with id: ${task.id}"))
+        }
+    }
+
+    override suspend fun createParentTask(task: Task): TaskUpdateResult {
+        tasks[task.id] = task
+        parentTasks.add(task.id)
+        return TaskUpdateResult.Success
+    }
+
+    override suspend fun createSubTask(parentId: String, task: Task): TaskUpdateResult {
+        tasks[task.id] = task
+        tasks[parentId]?.let {  parent ->
+            tasks[parentId] = parent.copy(
+                subTasks = parent.subTasks + task
+            )
+        } ?: return TaskUpdateResult.Failure(IllegalArgumentException("No task with id: $parentId"))
+        return TaskUpdateResult.Success
+    }
+}
